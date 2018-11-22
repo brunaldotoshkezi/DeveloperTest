a2d3680bab29fb94703cf973ec80910c2d0f14fb
aldo <aldo@LENOVO-PC>
2018-11-22 23:44:55 +0100

    Update Code

--- DeveloperTest1/Program.cs
+++ DeveloperTest1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DeveloperTest1
 {
@@ -10,41 +11,69 @@ namespace DeveloperTest1
             string haystack = "vnk2435kvabco8awkh125kjneytbcd12qjhb4acd123xmnbqwnw4t";
             string needle = "abcd1234";
             int threshold = 3;
-
-            byte[] pattern = new byte[] { 12, 3, 5, 76, 8, 0, 6, 125 };
-
-            byte[] toBeSearched = new byte[] { 23, 36, 43, 76, 125, 56, 34, 234, 12, 3, 5, 76, 8, 0, 6, 125, 234, 56, 211, 122, 22, 4, 7, 89, 76, 64, 12, 3, 5, 76, 8, 0, 6, 125 };
-
-            //List<int> positions = SearchBytePattern(pattern, toBeSearched);
-
-            //foreach (var item in positions)
-            //{
-            //    Console.WriteLine("Pattern matched at pos {0}", item);
-            //}
+            var results = Enumerable.Range(0, 1 << needle.Length)
+            .Select(e => string.Join(string.Empty, Enumerable.Range(0, needle.Length).Select(b => (e & (1 << b)) == 0 ? (char?)null : needle[b])));
+            List<string> values = new List<string>();
+            foreach (var result in results)
+            {
+                if (result.ToString().Length >= threshold && needle.Contains(result.ToString()))
+                {
+                    values.Add(result);
+                }
+            }
+            
+             SearchString(haystack, values,needle.Length);
 
         }
 
-        //static public List<int> SearchBytePattern(byte[] pattern, byte[] bytes)
-        //{
-        //    List<int> positions = new List<int>();
-        //    int patternLength = pattern.Length;
-        //    int totalLength = bytes.Length;
-        //    byte firstMatchByte = pattern[0];
-        //    for (int i = 0; i < totalLength; i++)
-        //    {
-        //        if (firstMatchByte == bytes[i] && totalLength - i >= patternLength)
-        //        {
-        //            byte[] match = new byte[patternLength];
-        //            Array.Copy(bytes, i, match, 0, patternLength);
-        //            if (match.SequenceEqual<byte>(pattern))
-        //            {
-        //                positions.Add(i);
-        //                i += patternLength - 1;
-        //            }
-        //        }
-        //    }
-        //    return positions
 
-        //}
+       
+        public static void SearchString(string str, List<string> values , int l)
+        {
+	        List<int> retVal = new List<int>();
+                    foreach (string pat in values)
+                    {
+                        int m = pat.Length;
+                        int n = str.Length;
+
+                        int[] badChar = new int[256];
+
+                        BadCharHeuristic(pat, m, ref badChar);
+
+                        int s = 0;
+                        while (s <= (n - m))
+                        {
+                            int j = m - 1;
+
+                            while (j >= 0 && pat[j] == str[s + j])
+                                --j;
+
+                            if (j < 0)
+                            {
+                                retVal.Add(s);
+                                Console.WriteLine("sequence of length = " + pat.Length + " found at haystack offset " + s + ", needle offset " + (l - pat.Length).ToString());
+                                s += (s + m < n) ? m - badChar[str[s + m]] : 1;
+                            }
+                            else
+                            {
+                                s += Math.Max(1, j - badChar[str[s + j]]);
+                            }
+                        }
+                    }
+        }
+
+        private static void BadCharHeuristic(string str, int size, ref int[] badChar)
+        {
+            int i;
+
+            for (i = 0; i < 256; i++)
+                badChar[i] = -1;
+
+            for (i = 0; i < size; i++)
+                badChar[(int)str[i]] = i;
+        }
+       
     }
+
 }
+
